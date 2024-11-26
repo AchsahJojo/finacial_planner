c833970ea3270b9715c278bf7aed0444e0dacbea
Jesus Garcia Loyola <116201865+JesusG2022@users.noreply.github.com>
2024-11-25 16:02:07 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </div>
     </Router>
